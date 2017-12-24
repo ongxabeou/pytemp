235bf5215843c564d3e63fd133b5f245659d1fd7
anhlt1980 <anhlt588@gmail.com>
2017-12-24 14:11:25 +0700

    fix bug thread_pool library

--- src/libs/thread_pool/__init__.py
+++ src/libs/thread_pool/__init__.py
@@ -6,6 +6,7 @@
     Date created: 2017/12/22
 """
 import datetime
+import hashlib
 import sys
 
 # để có thể chạy được cho cả python 2 và 3
@@ -22,54 +23,26 @@ else:
 from threading import Thread
 
 
-class Worker(Thread):
-    """ Thread thực hiện nhiệm vụ từ một hàng đợi nhiệm vụ nhất định """
-
-    def __init__(self, tasks, logger):
-        Thread.__init__(self)
-        self.tasks = tasks
-        self.logger = logger
-        self.daemon = True
-        self.start()
-
-    def run(self):
-        while True:
-            func, args, kargs = self.tasks.get()
-            try:
-                func(*args, **kargs)
-            except:
-                # Một trường hợp ngoại lệ đã xảy ra trong thread này
-                if self.logger is None:
-                    traceback.print_exc(file=sys.stdout)
-                else:
-                    line = '=================================================================='
-                    if args is ():
-                        self.logger.exception('%s\n%s :: %s exception occurred' %
-                                              (line, str(datetime.datetime.now()), func.__name__))
-                    else:
-                        self.logger.exception('%s\n%s :: %s exception occurred with param %r' %
-                                              (line, str(datetime.datetime.now()), func.__name__, args))
-            finally:
-                # Đánh dấu công việc này là xong, dù có ngoại lệ xảy ra hay không
-                self.tasks.task_done()
-
-
 class ThreadPool:
     """ Pool của Thread tiêu thụ nhiệm vụ từ một hàng đợi """
 
     def __init__(self, num_workers, logger=None):
         self.tasks = Queue(num_workers)
+        self.results = {}
         for _ in range(num_workers):
-            Worker(self.tasks, logger)
+            self.Worker(self.tasks, logger, self.results)
 
     def add_task(self, func, *args, **kargs):
         """ Thêm một tác vụ vào hàng đợi """
         self.tasks.put((func, args, kargs))
+        return self.Worker.get_function_id(func, args, kargs)
 
     def map(self, func, args_list):
         """ Thêm một danh sách các nhiệm vụ vào hàng đợi """
+        ids = []
         for args in args_list:
-            self.add_task(func, *args)
+            ids.append(self.add_task(func, *args))
+        return ids
 
     def wait_all_tasks_done(self):
         """ Chờ hoàn thành tất cả các nhiệm vụ trong hàng đợi """
@@ -80,9 +53,63 @@ class ThreadPool:
 
         @wraps(f)
         def decorated(*args, **kargs):
-            self.add_task(f, *args, **kargs)
+            return self.add_task(f, *args, **kargs)
 
         return decorated
+
+    def clear(self):
+        self.results.clear()
+
+    class Worker(Thread):
+        """ Thread thực hiện nhiệm vụ từ một hàng đợi nhiệm vụ nhất định """
+
+        def __init__(self, tasks, logger, results):
+            Thread.__init__(self)
+            self.results = results
+            self.tasks = tasks
+            self.logger = logger
+            self.daemon = True
+            self.start()
+
+        def run(self):
+            """ hàm thực hiện nhiệm vụ,
+                nếu nhiệm vụ đã từng thực hiện rồi thi không thực hiện nữa
+                nếu chưa thì thực hiện nhiệm vụ và ghi kết quả vào result
+                quá trình thực hiện nếu lỗi được ghi log nếu logger != None
+            """
+            while True:
+                func, args, kargs = self.tasks.get()
+                try:
+                    id = self.get_function_id(func, args, kargs)
+                    if id in self.results:
+                        return self.results[id]
+                    else:
+                        result = func(*args, **kargs)
+                    self.results[id] = result
+                except SystemExit:
+                    pass
+                except:
+                    # Một trường hợp ngoại lệ đã xảy ra trong thread này
+                    if self.logger is None:
+                        raise
+                    else:
+                        line = '=================================================================='
+                        if args is ():
+                            self.logger.exception('Exception in thread %s\n%s :: %s' %
+                                                  (line, str(datetime.datetime.now()), func.__name__))
+                        else:
+                            self.logger.exception('Exception in thread %s\n%s :: %s with param %r' %
+                                                  (line, str(datetime.datetime.now()), func.__name__, args))
+                finally:
+                    # Đánh dấu công việc này là xong, dù có ngoại lệ xảy ra hay không
+                    self.tasks.task_done()
+
+        @staticmethod
+        def get_function_id(func, args, kargs=None):
+            identify = "{function} {agrs} ".format(
+                function=func.__name__,
+                agrs="args %r kargs %s" % (args, kargs))
+            return hashlib.md5(identify.encode('utf-8')).hexdigest()
 
 
 # ------------------Test------------------------
@@ -101,6 +128,7 @@ if __name__ == "__main__":
     def wait_delay(id_w, time):
         print("(%d)id sleeping for (%d)sec" % (id_w, time))
         sleep(time)
+        return time * 100
 
 
     @pool.thread
@@ -133,10 +161,13 @@ if __name__ == "__main__":
     # Thêm các công việc với số lượng lớn vào thread.
     # Hoặc bạn có thể sử dụng `pool.add_task` để thêm
     # các công việc đơn lẻ.
-    pool.map(wait_delay, delays)
+    func_ids = pool.map(wait_delay, delays)
     # thêm một nhiệm vụ đơn lẻ
-    pool.add_task(wait_delay, 16, 8)
+    func_id = pool.add_task(wait_delay, 16, 8)
 
     pool.add_task(func_error)
     # đợi cho đến khi tất cả nhiệm vụ được hoàn thành
     pool.wait_all_tasks_done()
+    # kiểm tra kết quả
+    print(func_ids)
+    print(pool.results[func_id])
