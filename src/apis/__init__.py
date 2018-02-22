f63e528e227a98f8e461c5080449aeaa49d2866f
anhlt1980 <anhlt588@gmail.com>
2018-02-22 22:59:44 +0700

    fix code

--- src/apis/__init__.py
+++ src/apis/__init__.py
@@ -38,11 +38,12 @@ def get_param_exception(errors):
 
 
 def build_response_message(data=None):
-    message = BaseMoError(LANG.MESSAGE_SUCCESS).get_message()
     log_mod = sys_conf.get_section_map(SECTION.LOGGING_MODE)
     if int(log_mod[LOGGING_MODE.LOG_FOR_REQUEST_SUCCESS]) == 1:
-        sys_conf.logger.debug('response: %s' % (data or message))
-    return data or message
+        message = BaseMoError(LANG.MESSAGE_SUCCESS).get_message()
+        sys_conf.logger.debug('response: %s' % data)
+        data = data or message
+    return data
 
 
 @app.errorhandler(404)
