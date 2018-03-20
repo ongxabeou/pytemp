c3e43991448bde96ba16eee8a3982467267b5e02
anhlt1980 <anhlt588@gmail.com>
2018-03-20 22:32:45 +0700

    refactor

--- src/controllers/bot_controller.py
+++ src/controllers/bot_controller.py
@@ -12,6 +12,7 @@ from src.libs.http_validator import Required, Length, Unicode, Range, In, Instan
 from src.libs.subscribe import subscribe
 from src.models import BOT_STRUCTURE, CONSUMER
 from src.models.bot_config_repository import BotConfigRepository
+from src.performers import SUBSCRIBE_LABEL
 
 
 class BotController(BaseController):
@@ -20,7 +21,7 @@ class BotController(BaseController):
         self.abort_if_param_none_or_empty(bot_id, 'bot_id')
         self.bot_id = bot_id
 
-    @subscribe(label='', entity_id_index=1)
+    @subscribe(label=SUBSCRIBE_LABEL.PUT_CUSTOMER, entity_id_index=1)
     def put(self, customer):
         self.abort_if_param_none_or_empty(customer, 'thông tin khách hàng')
 
@@ -36,10 +37,11 @@ class BotController(BaseController):
 
         return 'say hello, đây là project mẫu'
 
+    @subscribe(label=SUBSCRIBE_LABEL.DELETE_BOT_CONFIG, entity_id_index=1)
     def delete(self):
         return BotConfigRepository().delete(self.bot_id)
 
-    @subscribe(entity_id_index=1)
+    @subscribe(label=SUBSCRIBE_LABEL.UPDATE_BOT_CONFIG, entity_id_index=1)
     def set(self, bot_config):
         self.abort_if_param_none_or_empty(bot_config, 'bot config')
 
--- src/performers/__init__.py
+++ src/performers/__init__.py
@@ -1,6 +1,10 @@
 from src.libs.subscribe import SubscribeAssigner
 from src.performers.test_performer import TestPerformer
 
+SubscribeAssigner().add_performer(TestPerformer())
 
-def init_all_performer():
-    SubscribeAssigner().add_performer(TestPerformer())
+
+class SUBSCRIBE_LABEL:
+    UPDATE_BOT_CONFIG = 'update_bot_config'
+    PUT_CUSTOMER = 'put_customer'
+    DELETE_BOT_CONFIG = 'delete_bot_config'
